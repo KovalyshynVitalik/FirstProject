12376ea5918e7022d5403a7878bfa8b32ad77fb5
Byra1997 <avtoupr@gmail.com>
2019-07-31 21:22:24 +0300

    Initial Commit

---
+++ ConcertApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConcertApp
+//
+//  Created by Misha on 7/31/19.
+//  Copyright © 2019 Misha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
